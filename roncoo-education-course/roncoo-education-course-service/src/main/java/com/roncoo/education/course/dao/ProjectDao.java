a97f830f0e1c420bfa26cc55c9cd5adc659d02da
Mia <yuelima@usc.edu>
2023-04-02 20:48:12 -0700

    Revert "add project api"
    This reverts commit b67eeae164e8f08bea099726cc11d68b2f577ff7.

--- roncoo-education-course/roncoo-education-course-service/src/main/java/com/roncoo/education/course/dao/ProjectDao.java
+++
@@ -1,7 +0,0 @@
-package com.roncoo.education.course.dao;
-
-import com.roncoo.education.course.dao.impl.mapper.entity.Project;
-
-public interface ProjectDao {
-    public Project searchByProjectId(Long projectId);
-}
